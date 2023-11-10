554c2c4e4650e7d25a53ac0adcb627a0eced28e9
MD. MOINUL ISLAM <118617222+MOINUL909@users.noreply.github.com>
2023-11-10 11:28:14 +0600

    Add files via upload

---
+++ 1008.c
@@ -0,0 +1,19 @@
+/*MOINUL*/
+#include <stdio.h>
+
+int main() {
+    int NUMBER,WORKING;
+    float SALARY,B;
+    scanf("%d%d",&NUMBER,&WORKING);
+    scanf("%f",&B);
+    SALARY=WORKING*B;
+    printf("NUMBER = %d\n",NUMBER);
+    printf("SALARY = U$ %.2f\n",SALARY);
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
